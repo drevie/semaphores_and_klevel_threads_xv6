ba17bafe865fb16bfa31ef16bf6f91e055d12c57
Daniel Revie <daniel.revie1@gmail.com>
2016-11-19 11:00:50 -0500

    commit

--- proc.h
+++ proc.h
@@ -66,10 +66,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  // BEGIN CHANGES
-  // Indicates if thread
-  int isthread;
-  void *stack;
+  int stack; 
 };
 
 // Process memory is laid out contiguously, low addresses first:
